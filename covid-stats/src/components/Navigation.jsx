f1a8d1c34019f95a161c23e2fced18bdd9e7b5e5
Njeri-Kamau <njeerikamau2@gmail.com>
2024-05-23 16:58:54 +0300

    Navigation bar

---
+++ covid-stats/src/components/Navigation.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Navigation () {
+  return (
+    <nav className='nav'>
+    <h2>COVID STATS</h2>
+        <ul>
+            <a>Learn</a>
+            <a>About</a>
+            <a>Help</a>
+        </ul>
+    </nav>
+  )
+}
